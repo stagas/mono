50ec9d7de0f26ff0dbcbcc6bcda2045e0127f59c
stagas <gstagas@gmail.com>
2022-07-31 16:18:58 +0300

    feature: add fill offset

--- src/lib/vm.ts
+++ src/lib/vm.ts
@@ -1,8 +1,9 @@
-import type { LinkerConfig } from '../linker'
-import { Type, W, max } from '../typed'
 import { Arg } from '..'
+import type { LinkerConfig } from '../linker'
+import { max, Type, W } from '../typed'
 
-export const env = (config: LinkerConfig) => `;;wasm
+export const env = (config: LinkerConfig) =>
+  `;;wasm
   (import "env" "memory"
     (memory
       ${config.memory.initial}
@@ -23,23 +24,37 @@ export const env = (config: LinkerConfig) => `;;wasm
 // TODO: length should be the global_buffer_length
 
 export interface VM {
-  fill(frame: number, length: number, ...args: number[]): void
+  fill(frame: number, offset: number, end: number, ...args: number[]): void
 }
 
-export const fill = ({ type, params }: { type: Type; params: Arg[] }) => `;;wasm
+export const fill = ({ type, params }: { type: Type; params: Arg[] }) =>
+  `;;wasm
   (func $fill (export "fill")
     ;; song position starting frame
     (param $frame i32)
-    ;; buffer length to fill
-    (param $length i32)
+    ;; buffer offset
+    (param $offset i32)
+    ;; buffer end index exclusive
+    (param $end i32)
     ;; ...params
     ${params.map(x => `(param $${x.id} ${max(Type.i32, x.type!)})`).join(' ')}
 
-    ;; buffer index position
-    (local $index i32)
+    (local $sample_time f32)
 
-    ;; index = 0
-    (local.set $index (i32.const 0))
+    (local.set $sample_time
+      (f32.div
+        (f32.const 1.0)
+        (global.get $sr)
+      )
+    )
+
+    ;; t = $frame / $sr
+    ;;(global.set $t
+    ;;  (f32.div
+    ;;    (f32.convert_i32_u (local.get $frame))
+    ;;    (global.get $sr)
+    ;;  )
+    ;;)
 
     ;; do
     (loop $loop
@@ -48,30 +63,25 @@ export const fill = ({ type, params }: { type: Type; params: Arg[] }) => `;;wasm
       ;; run initializers
       (call $__begin__)
 
-      ;; t = $frame / $sr
-      (global.set $t
-        (f32.div
-          (f32.convert_i32_u (local.get $frame))
-          (global.get $sr)
-        )
-      )
-
-      ;; buffer[$index] =
-      (f32.store (i32.shl (local.get $index) (i32.const 2))
+      ;; buffer[$offset] =
+      (f32.store (i32.shl (local.get $offset) (i32.const 2))
         ;; f(...params)
         ${W(type) < W(Type.f32) ? '(f32.convert_i32_s ' : ''}
         (call $f ${params.map(x => `(local.get $${x.id})`).join(' ')})
         ${W(type) < W(Type.f32) ? ')' : ''}
       )
 
-      ;; $index++
-      (local.set $index (i32.add (local.get $index) (i32.const 1)))
+      ;; $t += $sample_time
+      (global.set $t (f32.add (global.get $t) (local.get $sample_time)))
+
+      ;; $offset++
+      (local.set $offset (i32.add (local.get $offset) (i32.const 1)))
 
       ;; $frame++
       (local.set $frame (i32.add (local.get $frame) (i32.const 1)))
 
-      ;; if ($index !== $length) continue $loop
-      (br_if $loop (i32.ne (local.get $index) (local.get $length)))
+      ;; if ($offset !== $end) continue $loop
+      (br_if $loop (i32.ne (local.get $offset) (local.get $end)))
     )
   )
 `
